f2b356bbfc36ff4ffa71fc841c228bcf8b087925
Gr2dd3 <gradde23@gmail.com>
2022-12-23 09:25:25 +0100

    Added comment
    Check for database data

--- Program.cs
+++ Program.cs
@@ -8,5 +8,22 @@ namespace Webshop
         {
             
         }
+
+
+        //          Check if data already exist in database
+                    
+        //            foreach (string appearence in character.Appearances)
+        //            {
+        //                Appearence app = appearenceList.ToList().SingleOrDefault(a => a.Name == appearence);
+        //                if (app == null)
+        //                {
+        //                    app = new Appearence()
+        //                    {
+        //                        Name = appearence
+        //                    };
+        //                    appearenceList.Add(app);
+        //                }
+        //                newChar.Appearences.Add(app);
+        //            }
     }
 }
