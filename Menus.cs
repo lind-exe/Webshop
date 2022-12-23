eb0ad39862627f9bf1944b2a72ec5cd47f4a5e0e
andreastollmar <andreastollmar@gmail.com>
2022-12-23 08:48:09 +0100

    creat menus cs
    la till menyclass och la in en snabb meny från mickes lektion som vi kan använda oss av överallt

---
+++ Menus.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop
+{
+    internal class Menus
+    {
+        enum MenuList
+        {
+            Log_in,
+            New_Customer,
+            Browse_Shop,
+            Search_Product,
+            Exit_Shop
+
+        }
+
+        public static void Show()
+        {
+            bool go = true;
+            while(go)
+            {
+
+                foreach(int i in Enum.GetValues(typeof(MenuList)))
+                {
+                    Console.WriteLine($"{i}. {Enum.GetName(typeof(MenuList), i).Replace("_", " ")}");
+                }
+            }
+
+            int nr;
+            MenuList menu = (MenuList)99; //Default
+            if(int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out nr))
+            {
+                menu = (MenuList)nr;
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+
+            switch(menu)
+            {
+                case MenuList.Log_in:
+                    break;
+                case MenuList.New_Customer:
+                    break;
+                case MenuList.Browse_Shop:
+                    break;
+                case MenuList.Search_Product:
+                    break;
+                case MenuList.Exit_Shop:
+                    break;
+            }
+        }
+
+    }
+}
