443e9dfcb823831bae39ea63db2d72f0eae94d9a
Gr2dd3 <gradde23@gmail.com>
2023-01-10 10:36:44 +0100

    Welcome method

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System.Net.Http.Headers;
-using System.Reflection.Metadata;
+﻿//HardCodedValues.AllInserts();
 using Webshop.Methods;
-//HardCodedValues.AllInserts();
+
 namespace Webshop
 {
-    
+
     internal class Program
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Methods.Menus.Show("Main");
+=======
+<<<<<<< HEAD
+            //HardCodedValues.AllInserts();
+            Helpers.Welcome();
+
+
+
+=======
+            Methods.Menus.Show("Main");
+>>>>>>> 699fea7406f2c9c310cd2276c09d20f12c90a4ca
+>>>>>>> Stashed changes
         }
+
+
+
     }
 }
